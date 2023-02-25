54c16e1b46ce1961d6509dc61d759e5b44599660
Ralphine <r.deroo99@gmail.com>
2023-02-25 17:33:18 +0100

    Add tailwind, sidebar and canvas

---
+++ src/layout/Layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Canvas from './Canvas.svelte';
+	import Sidebar from './Sidebar.svelte';
+</script>
+
+<div class="flex h-screen">
+	<div class="w-1/5 shadow-lg p-6">
+		<Sidebar />
+	</div>
+
+	<div class="grow p-6">
+		<Canvas />
+	</div>
+</div>
---
+++ src/layout/Sidebar.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	const components: string[] = ['button', 'category', 'card'];
+</script>
+
+<h1>Sidebar</h1>
+<div class="flex flex-col items-stretch gap-2">
+	{#each components as component}
+		<button class="hover:bg-slate-100 rounded-md py-2 pl-4 pr-2 text-left"
+			>{component.charAt(0).toUpperCase() + component.slice(1)}
+		</button>
+	{/each}
+</div>
